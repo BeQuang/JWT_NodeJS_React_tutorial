affd42f30c01919e85ce1dd02dbad3abcf7e7f4f
BEQUANG2409 <21522507@gm.uit.edu.vn>
2024-10-22 15:36:21 +0700

    Thay đổi connection thành bất đồng bộ, render dữ liệu nhận được ra view.

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,14 +1,15 @@
-import mysql from "mysql2";
 import bcrypt from "bcryptjs";
+import mysql from "mysql2/promise";
+import bluebird from "bluebird";
 
 const salt = bcrypt.genSaltSync(10);
 
 // Create the connection to the database
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "jwt_nodejs_react",
-});
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   database: "jwt_nodejs_react",
+// });
 
 const hashUserPassword = (userPassword) => {
   return bcrypt.hashSync(userPassword, salt);
@@ -28,14 +29,30 @@ const createNewUser = (email, password, username) => {
   );
 };
 
-const getUserList = () => {
-  let users = [];
-  connection.query("SELECT * FROM users", function (err, result) {
-    if (err) {
-      console.log(err);
-    }
-    console.log("check user list >>>> ", result);
+const getUserList = async () => {
+  const connection = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "jwt_nodejs_react",
+    Promise: bluebird,
   });
+
+  let users = [];
+  //   connection.query("SELECT * FROM users", function (err, result) {
+  //     if (err) {
+  //       console.log(err);
+  //       return users;
+  //     }
+  //     users = result;
+  //     return users;
+  //   });
+
+  try {
+    const [rows, fields] = await connection.execute("SELECT * FROM users");
+    return rows;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = {
