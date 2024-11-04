6c22b774e20237e96bbfe0748093927c5d95b446
BEQUANG2409 <21522507@gm.uit.edu.vn>
2024-11-04 14:44:13 +0700

    Fixbug CORS (Cross-Origin Resource Sharing)

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,5 +1,6 @@
 import express from "express";
 import homeController from "../controller/homeController";
+import apiController from "../controller/apiController";
 
 const router = express.Router();
 
@@ -16,6 +17,9 @@ const initWebRoutes = (app) => {
   router.post("/delete-user/:id", homeController.handleDeleteUser);
   router.get("/update-user/:id", homeController.getUpdateUserPage);
   router.post("/user/update-user", homeController.handleUpdateUser);
+
+  // path API
+  router.get("/api/test-api", apiController.testAPI);
 
   return app.use("/", router);
 };
